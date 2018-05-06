libgme/game-music-emu

commit 205290614cdc057541b26adeea05a9d45993f860

nsfe: Sanity check block header size before reading.
Addresses a crash and potential out-of-bounds mem access in the NSFE
emulator, in issue #14.

This is only the barest fix for the current issue, but the codebase
likely has similar flaws, which I will address (to extent I can find)
with a separate patch.

gme/Nsfe_Emu.cpp
@@ -134,6 +134,9 @@ blargg_err_t Nsfe_Info::load( Data_Reader& in, Nsf_Emu* nsf_emu )
 		RETURN_ERR( in.read( block_header, sizeof block_header ) );
 		blargg_long size = get_le32( block_header [0] );
 		blargg_long tag  = get_le32( block_header [1] );
+
+		if ( size <= 0 )
+			return "Corrupt file";
 		
 		//debug_printf( "tag: %c%c%c%c\n", char(tag), char(tag>>8), char(tag>>16), char(tag>>24) );
 		

commit 63119537125e0241e68736d33128edbc2a64e13f

nsfe: Some blocks can have a 0-sized header, don't fail for those.
This fixes a regression introduced by commit
205290614cdc057541b26adeea05a9d45993f860, where we added a check for
potentially malicious NSFE file dumps to avoid crashes or potential OOB
mem access.

It seems some valid files can have 0-sized blocks, so let those through
the first check.  This allows the testcases for Issue #22 to pass while
still rejecting the corrupt file on Issue #14.

Fixes Issue #22.

gme/Nsfe_Emu.cpp
@@ -135,7 +135,7 @@ blargg_err_t Nsfe_Info::load( Data_Reader& in, Nsf_Emu* nsf_emu )
 		blargg_long size = get_le32( block_header [0] );
 		blargg_long tag  = get_le32( block_header [1] );
 
-		if ( size <= 0 )
+		if ( size < 0 )
 			return "Corrupt file";
 		
 		//debug_printf( "tag: %c%c%c%c\n", char(tag), char(tag>>8), char(tag>>16), char(tag>>24) );