e76bdc0cb916e79aa540290e6edd0c445879d3ba
Michael Pyne <mpyne@purinchu.net>
2020-02-24 20:27:28 -0500

    Bump version in preparation for tagging/release.
    It's been 2 years and nearly 140 commits since the last release, and we
    have a pending pull request that would necessitate an API change.
    
    So let's make a release of what we have and then open up master for
    API changes, build system improvements, and C++ updates.
    
    Some notable improvements (nearly all from external contributors!):
    - Better support dodgy SPC rips
    - multichannel stereo (only SPC for now)
    - C++11 compilers now required
    - Compressed RAM state support (incl. VGZ)
    - Dual YM2612 support for VGM
    - Added MAME and Nuked OPN2 emulators for YM2612 in addition to GENS
    - Support for static builds and macOS framework builds
    - Improvements to the demo graphical player (SDL2 support, 60fps)
    - Music tracks can be set to ignore playback length metadata (to loop forever)
    - Read M3U files that have detailed timings

--- gme/gme.h
+++ gme/gme.h
@@ -1,6 +1,6 @@
 /* Game music emulator library C interface (also usable from C++) */
 
-/* Game_Music_Emu 0.6.2 */
+/* Game_Music_Emu 0.6.3 */
 #ifndef GME_H
 #define GME_H
 
@@ -10,7 +10,7 @@
 	extern "C" {
 #endif
 
-#define GME_VERSION 0x000602 /* 1 byte major, 1 byte minor, 1 byte patch-level */
+#define GME_VERSION 0x000603 /* 1 byte major, 1 byte minor, 1 byte patch-level */
 
 /* Error string returned by library functions, or NULL if no error (success) */
 typedef const char* gme_err_t;
