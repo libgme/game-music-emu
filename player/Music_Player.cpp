a465e8c61aa75cc62b8a3f0dc3f6bce4ec7046d0
Michael Pyne <mpyne@purinchu.net>
2020-03-04 22:48:50 -0500

    Bump .so version and fix player for the updated gme_set_fade.

--- player/Music_Player.cpp
+++ player/Music_Player.cpp
@@ -116,7 +116,7 @@ gme_err_t Music_Player::start_track( int track )
 		
 		if ( track_info_->length <= 0 )
 			track_info_->length = (long) (2.5 * 60 * 1000);
-		gme_set_fade( emu_, track_info_->length );
+		gme_set_fade( emu_, track_info_->length, 8000 );
 		
 		paused = false;
 		sound_start();
@@ -181,7 +181,7 @@ void Music_Player::mute_voices( int mask )
 
 void Music_Player::set_fadeout( bool fade )
 {
-	gme_set_fade( emu_, fade ? track_info_->length : -1 );
+	gme_set_fade( emu_, fade ? track_info_->length : -1, 8000 );
 }
 
 void Music_Player::fill_buffer( void* data, sample_t* out, int count )
